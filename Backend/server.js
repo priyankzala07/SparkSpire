18fcaefbd5a050b373fba45da7d3dd2d96ed32a4
Priyank <priyankzala07@gmail.com>
2026-07-17 11:02:13 +0530

    commited

--- Backend/server.js
+++ Backend/server.js
@@ -13,14 +13,18 @@ require('dotenv').config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
-app.use(cors());
+app.use(cors())
+// app.use(cors({
+//     origin: 'https://your-frontend-domain.com',
+//     credentials: true
+// }));
 app.use('/api/auth' , AuthRoute)
 app.use('/api/bookings' , BookingRoute)
 app.use('/api/events' , EventRoute)
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     app.listen(process.env.PORT, () => {
-        console.log("Server created");
+        console.log("Server connected and running on port " + process.env.PORT);
     });
 })
 .catch(console.error);
@@ -28,4 +32,7 @@ app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
+app.listen(process.env.PORT, () => {
+        console.log("Server created");
 });
