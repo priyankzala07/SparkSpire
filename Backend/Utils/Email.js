afaead75bd152047949c70cbfc531d382200be5b
Priyank <priyankzala07@gmail.com>
2026-07-17 14:36:39 +0530

    Add TLS configuration to email transporter

--- Backend/Utils/Email.js
+++ Backend/Utils/Email.js
@@ -13,6 +13,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+    family: 4   // force IPv4
+  }
 });
 
 transporter.verify((error, success) => {
