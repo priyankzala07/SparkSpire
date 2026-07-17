2064d404e7be3eb0d3f218f3d1a0fa5b502a2200
Priyank <priyankzala07@gmail.com>
2026-07-17 21:47:17 +0530

    Change email transporter settings to use port 587

--- Backend/Utils/Email.js
+++ Backend/Utils/Email.js
@@ -19,8 +19,8 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
