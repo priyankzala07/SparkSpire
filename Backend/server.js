afc85560dad34490ce8e36b526740a72653c8bcc
Priyank <priyankzala07@gmail.com>
2026-07-17 11:38:22 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -13,11 +13,10 @@ require('dotenv').config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
-app.use(cors())
-// app.use(cors({
-//     origin: 'https://your-frontend-domain.com',
-//     credentials: true
-// }));
+app.use(cors({
+    origin: 'https://sparkspire.onrender.com/',
+    credentials: true
+}));
 app.use('/api/auth' , AuthRoute)
 app.use('/api/bookings' , BookingRoute)
 app.use('/api/events' , EventRoute)
